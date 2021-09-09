943a53e196ec2a95ac087bb1a6b4a89c733744c2
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-08 22:10:49 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Routes from "./routes";
+import Header from './components/Header';
+
 function App() {
   return (
+    <>
+    <Header/>
     <Routes/>
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './header.css';
+
+export default function Headers(){
+    return(
+        <h1>Header</h1>
+    )
+}
